bed1af36bf155492c2ce20bc9071db3483a13905
D-benlala <75784662+D-benlala@users.noreply.github.com>
2021-02-10 12:49:13 +0800

    update

---
+++ 2021_02_10/2021_02_10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2021_02_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
