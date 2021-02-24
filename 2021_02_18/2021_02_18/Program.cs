6a74936f9cb3723e9f30a903eb15a76c7cf45182
D-benlala <75784662+D-benlala@users.noreply.github.com>
2021-02-24 19:54:43 +0800

    update

---
+++ 2021_02_18/2021_02_18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2021_02_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int:{0}", sizeof(int));
+            Console.ReadLine();
+            object a;
+            a = 1;
+            dynamic b = 2;
+            String str = "ghb.com";
+        }
+    }
+}
