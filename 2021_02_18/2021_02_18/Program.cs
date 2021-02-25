cdaa8deb47294f698b9ea4d71cea00572eb45aa7
D-benlala <75784662+D-benlala@users.noreply.github.com>
2021-02-25 23:02:50 +0800

    update

--- 2021_02_18/2021_02_18/Program.cs
+++ 2021_02_18/2021_02_18/Program.cs
@@ -8,10 +8,21 @@ namespace _2021_02_18
         {
             Console.WriteLine("Size of int:{0}", sizeof(int));
             Console.ReadLine();
-            object a;
-            a = 1;
-            dynamic b = 2;
-            String str = "ghb.com";
+
+            bool a = false;
+            byte b = 1;
+            char c = '\0';
+            decimal d = 2;
+            double e = 0.00D;
+            float f = 100;
+            int g = 123;
+            long h = 321;
+            sbyte i = 54;
+            short j = 45;
+            uint k = 789;
+            ulong l= 1;
+            ushort m= 1;
+
         }
     }
 }
